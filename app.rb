880bc3aaf3484d80ec6c424eff8daeb277c939c0
kashiwa-mochi <wakabayashikota@gmail.com>
2021-05-18 13:24:07 +0000

    first commit

--- app.rb
+++ app.rb
@@ -1,33 +1,43 @@
-require 'bundler/setup'
-Bundler.require
-require 'sinatra/reloader' if development?
-require './models/count.rb'
+require 'bundler/setup' 
+Bundler.require 
+require 'sinatra/reloader' 
+if development?
+  require './models/count.rb' 
 
-before do
-  if Count.all.size == 0
-    Count.create(number: 0)
+  before do 
+    if Count.all.size == 0
+        Count.create(number: 0) 
+    end
+  end 
+
+  get '/' do 
+    'こんばんはー' 
+  end 
+
+  get '/count' do 
+    @number = Count.first.number
+    erb:index 
+  end 
+
+  post '/plus' do
+    count = Count.first
+    count.number = count.number + 1 
+    count.save
+    redirect '/count'
+  end 
+
+  post '/minus' do
+    count = Count.first 
+    count.number = count.number - 1
+    count.save 
+    redirect '/count' 
+  end 
+
+  post '/clear' do
+    count = Count.find(1)
+    count.number = count.number-count.number
+    count.save
+    redirect '/count'
   end
-end
 
-get '/' do
-  'こんばんはー'
-end
-
-get '/count' do
-  @number = Count.first.number
-  erb :index
-end
-
-post '/plus' do
-  count = Count.first
-  count.number = count.number + 1
-  count.save
-  redirect '/count'
-end
-
-post '/minus' do
-  count = Count.first
-  count.number = count.number - 1
-  count.save
-  redirect '/count'
 end
